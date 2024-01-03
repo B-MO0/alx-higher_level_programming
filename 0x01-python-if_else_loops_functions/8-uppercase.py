67e1d75225d9375e6a60050438f323bd6436c2ed
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2024-01-03 12:58:01 -0800

    GG

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(ch) <= 122:
+            i = i(ord(ch) - 32)
+        print("{:s}".format(i), end='')
+    print('\n', end="")
