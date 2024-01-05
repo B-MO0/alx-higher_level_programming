df8d8cc1dbf4ffb1d328672bd13310a3594fbae0
B-MO0 <144631945+B-MO0@users.noreply.github.com>
2024-01-05 19:07:34 +0100

    lls

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     add = 0
-    for s in sys.argv[1:]:
+    for s in argv[1:]:
         add += int(s)
     print("{:d}".format(add))
