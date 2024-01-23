1c89c58c14ad636890bce6da7b7590bb69085532
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2024-01-23 09:24:58 -0800

    dffd

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,10 +13,11 @@ class Square:
                 self.__size = size
         else:
             raise TypeError('size must be an integer')
-     def area(self):
-         """
-         calculates the area of sqr
-         Returns:
-             the area of square
-         """
-         return self.__size ** 2
+
+    def area(self):
+        """
+        calculates the area of sqr
+        Returns:
+            the area of square
+        """
+        return self.__size ** 2
