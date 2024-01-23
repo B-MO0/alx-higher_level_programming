c78d7911b687445924f84cfb7d7434c092217f24
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2024-01-23 09:13:14 -0800

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""define my class """
+
+
+class Square:
+    """ my class atrs """
+    def __init__(self, size=0):
+        """initialize atrs"""
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
+     def area(self):
+         """ returns the area of square """
+         return size ** 2
