795575428a25a58796bc9395f1ed971714248cd8
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2024-01-23 08:39:05 -0800

    ll

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define my class square with atr size"""
+
+
+class Square:
+    """ adding private atr __size """
+    def __init__(self, size):
+        """ initialization of size """
+        self.__size = size
