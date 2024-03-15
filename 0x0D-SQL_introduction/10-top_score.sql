de3c2ad2fe6ef591f9bf328bb74c3231861f3518
B-MO0 <144631945+B-MO0@users.noreply.github.com>
2024-03-15 03:19:59 +0000

    sql is fun

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- List all records in descending order
+SELECT name, score
+FROM second_table
+ORDER BY score desc;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- Updates entry based on name
+UPDATE second_table
+SET
+	score = 10
+WHERE
+	name = "Bob";
